619723b83f7e1d8de897d4b0647a241f884029bf
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-16 17:20:20 +0000

    Scaffold User model

--- My Everything List/Data/MyEverythingListContext.cs
+++ My Everything List/Data/MyEverythingListContext.cs
@@ -1,13 +1,16 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using My_Everything_List.Models;
 
 namespace My_Everything_List.Data;
 
 public class MyEverythingListContext : DbContext
 {
-    public MyEverythingListContext (DbContextOptions<MyEverythingListContext> options)
+    public MyEverythingListContext(DbContextOptions<MyEverythingListContext> options)
         : base(options)
     {
     }
 
-    public DbSet<Models.Film> Film { get; set; } = default!;
+    public DbSet<Film> Film { get; set; } = default!;
+
+    public DbSet<User> User { get; set; } = default!;
 }
