5e63b1f919a2ee843ef666795f442326fd1508bc
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-23 14:35:14 +0000

    Create TMDB Endpoints class

---
+++ My Everything List/Services/TmdbService/TmdbEndpoint.cs
@@ -0,0 +1,8 @@
+namespace My_Everything_List.Services.TmdbService;
+
+internal static class TmdbEndpoint
+{
+    public static string GetFilm(int movieId) => $"movie/{movieId}";
+    public static string SearchFilm(string query) => $"search/movie?query={query}";
+    public static string Config = "configuration" ;
+}
