f7106248667e596b698921888e24fb5a0ffb199d
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-25 13:59:05 +0000

    Add Methods to LastFM service interface

--- My Everything List/Services/LastfmService/ILastfmService.cs
+++ My Everything List/Services/LastfmService/ILastfmService.cs
@@ -2,4 +2,7 @@ namespace My_Everything_List.Services.LastfmService;
 
 public interface ILastfmService
 {
+    Task<ArtistsSearchResults?> SearchArtists();
+    Task<AlbumsSearchResults?> SearchAlbums();
+    Task<SongsSearchResults?> SearchSongs();
 }
