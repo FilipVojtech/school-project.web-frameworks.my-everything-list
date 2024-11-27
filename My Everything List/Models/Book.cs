a391755d5c900963db3103bf64cc4c435875f087
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-27 16:58:59 +0000

    Save selected books to DB and load books on page load

--- My Everything List/Models/Book.cs
+++ My Everything List/Models/Book.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using My_Everything_List.Services.GoogleBooksService;
 
 namespace My_Everything_List.Models;
 
@@ -26,4 +27,32 @@ public class Book
 
     [Column("isbn")]
     public string? Isbn { get; set; }
+
+    public Book()
+    {
+        Title = string.Empty;
+        Authors = [];
+    }
+
+    public Book(string title, string? description, string[] authors, string? image, string? isbn)
+    {
+        Title = title;
+        Description = description;
+        Authors = authors;
+        Image = image;
+        Isbn = isbn;
+    }
+
+    public Book(Volume volume)
+    {
+        var volumeInfo = volume.volumeInfo;
+        Title = volumeInfo.title;
+        Description = volumeInfo.description;
+        Authors = volumeInfo.authors;
+        if (volumeInfo.imageLinks != null && volumeInfo.imageLinks.thumbnail != null)
+        {
+            Image = volumeInfo.imageLinks.thumbnail;
+        }
+        Isbn = volumeInfo.industryIdentifiers.FirstOrDefault()?.identifier ?? null;
+    }
 }
