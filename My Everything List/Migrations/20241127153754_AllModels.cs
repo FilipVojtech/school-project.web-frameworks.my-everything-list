393f7b4704fc1da79aac60952742460ae947da82
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-27 15:40:55 +0000

    Add migrations

---
+++ My Everything List/Migrations/20241127153754_AllModels.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyEverythingList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "genre",
+                table: "film");
+
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "film");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "film",
+                type: "TEXT",
+                maxLength: 512,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "genres",
+                table: "film",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<string>(
+                name: "image",
+                table: "film",
+                type: "TEXT",
+                maxLength: 32,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    authors = table.Column<string>(type: "TEXT", nullable: false),
+                    image = table.Column<string>(type: "TEXT", nullable: true),
+                    isbn = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "music_items",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    item_type = table.Column<int>(type: "INTEGER", nullable: false),
+                    image = table.Column<string>(type: "TEXT", nullable: true),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    artist = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_music_items", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "music_items");
+
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "film");
+
+            migrationBuilder.DropColumn(
+                name: "genres",
+                table: "film");
+
+            migrationBuilder.DropColumn(
+                name: "image",
+                table: "film");
+
+            migrationBuilder.AddColumn<string>(
+                name: "genre",
+                table: "film",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "price",
+                table: "film",
+                type: "decimal(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
