8a425c7926c6a5e7f2ee26cf6492558b91502da0
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-27 14:34:04 +0000

    Update Film model

--- My Everything List/Models/Film.cs
+++ My Everything List/Models/Film.cs
@@ -9,13 +9,38 @@ public class Film
     [Key]
     [Column("id")]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public int Id { get; init; }
 
-    [Column("title")] public string? Title { get; set; }
+    [Column("title")] [StringLength(64)] public string? Title { get; set; }
+
     [Column("release_date")] public DateOnly ReleaseDate { get; set; }
-    [Column("genre")] public string? Genre { get; set; }
 
-    [DataType(DataType.Currency)]
-    [Column("price", TypeName = "decimal(18, 2)")]
-    public decimal Price { get; set; }
+    [Column("genres")] public List<string> Genres { get; set; }
+
+    [Column("description")]
+    [StringLength(512)]
+    public string? Description { get; set; }
+
+    [Column("image")]
+    [DataType(DataType.Url)]
+    [StringLength(32)]
+    public string? Image { get; set; }
+
+    public Film(string? title, DateOnly releaseDate, string[] genres, string? description, string? image)
+    {
+        Title = title;
+        ReleaseDate = releaseDate;
+        Genres = [..genres];
+        Description = description;
+        Image = image;
+    }
+
+    public Film(Services.TmdbService.Film tmdbFilm)
+    {
+        Title = tmdbFilm.title;
+        ReleaseDate = DateOnly.Parse(tmdbFilm.release_date);
+        Genres = tmdbFilm.genres.Select(genre => genre.name).ToList();
+        Description = tmdbFilm.tagline;
+        Image = tmdbFilm.poster_path;
+    }
 }
