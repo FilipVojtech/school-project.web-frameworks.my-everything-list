0cce2399e28d0390e22c779423c310a3f479b9c3
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-28 00:29:55 +0000

    Implement equality tests

--- My Everything List.Tests/Models/AlbumTest.cs
+++ My Everything List.Tests/Models/AlbumTest.cs
@@ -7,10 +7,21 @@ namespace My_Everything_List.Tests.Models;
 [TestOf(typeof(Album))]
 public class AlbumTest
 {
-
-    [Test]
-    public void METHOD()
+    [Test(Description = "Albums equal when all of their properties are the same, regardless of image")]
+    public void Equals()
     {
-        
+        Album album1 = new Album("Album1", "/img1.jpg", "Artist1");
+        object album2 = new Album("Album1", "/img2.jpg", "Artist1");
+
+        Assert.That(album1.Equals(album2), Is.True);
+    }
+
+    [Test(Description = "Albums not equal when at least one of their properties is different")]
+    public void NotEquals()
+    {
+        Album album1 = new Album("Album1", "/img1.jpg", "Artist1");
+        object album2 = new Album("Album1", "/img1.jpg", "Artist2");
+
+        Assert.That(album1.Equals(album2), Is.False);
     }
 }
