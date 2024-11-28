c09e0c98b8693b8cc5569bee214fab2b609a4aa2
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-28 00:24:03 +0000

    Create Album test file

---
+++ My Everything List.Tests/Models/AlbumTest.cs
@@ -0,0 +1,16 @@
+using My_Everything_List.Models;
+using NUnit.Framework;
+
+namespace My_Everything_List.Tests.Models;
+
+[TestFixture]
+[TestOf(typeof(Album))]
+public class AlbumTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
