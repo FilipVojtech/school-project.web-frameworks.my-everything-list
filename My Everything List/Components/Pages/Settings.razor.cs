a901cd535c28f5ec00b4ce7c5b0b03be3149848e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-29 14:42:14 +0000

    Move page code to code-behind file

--- My Everything List/Components/Pages/Settings.razor.cs
+++ My Everything List/Components/Pages/Settings.razor.cs
@@ -4,6 +4,21 @@ namespace My_Everything_List.Components.Pages;
 
 public partial class Settings : ComponentBase
 {
+    private int? _id;
+
+    private string? _email;
+
+    private string? _role;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        _id = await UserService.GetId();
+        _email = await UserService.GetEmail();
+        _role = await UserService.GetRole();
+    }
+
     public void DeleteAccount()
     {
         Console.Out.WriteLine("Account Deleted");
