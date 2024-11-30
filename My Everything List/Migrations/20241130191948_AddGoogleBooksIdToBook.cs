b2c27c1cf52410cfbda2c7292cb54594ec63fce1
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-11-30 21:50:55 +0000

    Create Migration

---
+++ My Everything List/Migrations/20241130191948_AddGoogleBooksIdToBook.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyEverythingList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGoogleBooksIdToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "google_books_id",
+                table: "books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "google_books_id",
+                table: "books");
+        }
+    }
+}
