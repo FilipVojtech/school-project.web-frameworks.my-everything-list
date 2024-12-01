8d017c00c6bedf36e4c9491593d7fb43598f8ec2
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-01 23:54:05 +0000

    Add comments to LFM interface

--- My Everything List/Services/LastfmService/ILastfmService.cs
+++ My Everything List/Services/LastfmService/ILastfmService.cs
@@ -4,7 +4,18 @@ namespace My_Everything_List.Services.LastfmService;
 
 public interface ILastfmService
 {
+    /// <summary>
+    /// Search for artists
+    /// </summary>
+    /// <param name="artist">Query</param>
+    /// <returns>List of result artists</returns>
     Task<ArtistsSearchResults?> SearchArtists(string artist);
+
+    /// <summary>
+    /// Get artist data
+    /// </summary>
+    /// <param name="artist">The artist whose data should be returned</param>
+    /// <returns>The artist data</returns>
     Task<LfmArtist?> GetArtist(Artist artist);
 
     /// <summary>
@@ -13,9 +24,32 @@ public interface ILastfmService
     /// <returns>List of artists</returns>
     Task<object> GetTopArtists();
 
+    /// <summary>
+    /// Search for albums
+    /// </summary>
+    /// <param name="album">Query</param>
+    /// <returns>List of search result albums</returns>
     Task<AlbumsSearchResults?> SearchAlbums(string album);
+
+    /// <summary>
+    /// Get album info
+    /// </summary>
+    /// <param name="album">Album which data should be fetched</param>
+    /// <returns></returns>
     Task<LfmAlbum?> GetAlbum(Album album);
+
+    /// <summary>
+    /// Search for songs
+    /// </summary>
+    /// <param name="song">Query</param>
+    /// <returns>List of search result tracks</returns>
     Task<TracksSearchResults?> SearchSongs(string song);
+
+    /// <summary>
+    /// Get track info
+    /// </summary>
+    /// <param name="song">Track which data should be fetched</param>
+    /// <returns>Song</returns>
     Task<LfmTrack?> GetSong(Song song);
 
     /// <summary>
