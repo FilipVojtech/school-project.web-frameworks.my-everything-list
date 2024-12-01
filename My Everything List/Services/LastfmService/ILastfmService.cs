1581052bd56fbaf9889561261282049fff307c54
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-01 23:53:40 +0000

    Add top tracks and artists to LFM service interface

--- My Everything List/Services/LastfmService/ILastfmService.cs
+++ My Everything List/Services/LastfmService/ILastfmService.cs
@@ -6,8 +6,21 @@ public interface ILastfmService
 {
     Task<ArtistsSearchResults?> SearchArtists(string artist);
     Task<LfmArtist?> GetArtist(Artist artist);
+
+    /// <summary>
+    /// Get currently top artists
+    /// </summary>
+    /// <returns>List of artists</returns>
+    Task<object> GetTopArtists();
+
     Task<AlbumsSearchResults?> SearchAlbums(string album);
     Task<LfmAlbum?> GetAlbum(Album album);
     Task<TracksSearchResults?> SearchSongs(string song);
     Task<LfmTrack?> GetSong(Song song);
+
+    /// <summary>
+    /// Get currently top tracks
+    /// </summary>
+    /// <returns>List of top tracks</returns>
+    Task<object> GetTopTracks();
 }
