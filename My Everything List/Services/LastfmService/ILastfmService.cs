8f05273ddd136fbfed688772d4b87f09ee8c492e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-01 01:04:38 +0000

    Add Get artist method to Last.fm interface

--- My Everything List/Services/LastfmService/ILastfmService.cs
+++ My Everything List/Services/LastfmService/ILastfmService.cs
@@ -1,8 +1,11 @@
+using My_Everything_List.Models;
+
 namespace My_Everything_List.Services.LastfmService;
 
 public interface ILastfmService
 {
     Task<ArtistsSearchResults?> SearchArtists(string artist);
+    Task<lfmArtist?> GetArtist(Artist artist);
     Task<AlbumsSearchResults?> SearchAlbums(string album);
     Task<SongsSearchResults?> SearchSongs(string song);
 }
